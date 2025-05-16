f22ad319bd9f6f554ddfd2e97df0c7b627f52068
vitanixa <support@vitanixa.com>
2025-05-16 14:31:07 -0500

    Update Checkout.jsx

--- src/Checkout.jsx
+++ src/Checkout.jsx
@@ -12,11 +12,11 @@ export default function Checkout() {
         Pay with Stripe
       </a>
       <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank">
-        <input type="hidden" name="cmd" value="_xclick">
-        <input type="hidden" name="business" value="support@vitanixa.com">
-        <input type="hidden" name="item_name" value="Vitanixa SlimDetox">
-        <input type="hidden" name="amount" value="19.99">
-        <input type="hidden" name="currency_code" value="USD">
+        <input type="hidden" name="cmd" value="_xclick" />
+        <input type="hidden" name="business" value="support@vitanixa.com" />
+        <input type="hidden" name="item_name" value="Vitanixa SlimDetox" />
+        <input type="hidden" name="amount" value="19.99" />
+        <input type="hidden" name="currency_code" value="USD" />
         <button type="submit" className="bg-yellow-400 px-4 py-2 rounded w-full text-center">
           Pay with PayPal
         </button>
