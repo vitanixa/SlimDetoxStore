5518e1423d86179068a89dde36d84ac59ec20026
vitanixa <support@vitanixa.com>
2025-06-17 10:30:08 -0500

    new builds

---
+++ build/static/js/952.5920ae49.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkvitanixa_store=self.webpackChunkvitanixa_store||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.5920ae49.chunk.js.map
