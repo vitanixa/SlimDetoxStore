8a0333dc839e1923c9fcfb58a52471f9d3e28485
Alexis Nde <alexis.nde@pditechnologies.com>
2025-10-28 15:52:14 -0500

    working on cart and success pages

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -4,5 +4,3 @@ const supabaseUrl = "https://sjzdpvwzolilzdlxagsq.supabase.co";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNqemRwdnd6b2xpbHpkbHhhZ3NxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc3MTE5NDYsImV4cCI6MjA2MzI4Nzk0Nn0.Sntp9JoE4sSUO5M_D56JyBOSUaBK0f_QaW-mShlJiXk";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
-
-window.supabase = supabase;
